00eabe815398a64c7f7eb3cbb63aa4972904b908
Moa Sjögren <moasjgren@gmail.com>
2025-12-02 12:25:04 +0100

    moved nuxt version to folder, added vue router in /v-plain

---
+++ v-plain/src/App.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts"></script>
+
+<!-- Enkel setup för Vue router utifrån Joels basic-exempel. Lägg till sidor i /views och länka i /router -->
+
+<template>
+  <header>
+    <nav>
+      <RouterLink to="/">Home</RouterLink>
+      <RouterLink :to="{ name: 'about' }">About</RouterLink>
+    </nav>
+  </header>
+  <main>
+    <RouterView />
+  </main>
+</template>
+
+<style scoped>
+nav {
+  display: flex;
+  gap: 1rem;
+  padding: 1rem;
+  /* background-color: #f0f0f0; */
+  margin-bottom: 2rem;
+}
+
+.router-link-active {
+  font-weight: bold;
+  color: #42b883; /* Vue green */
+  text-decoration: underline;
+}
+</style>
