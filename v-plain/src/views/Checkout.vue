6fb348888dc270767ce50b3f9519c44f4c190a86
Moa Sjögren <moasjgren@gmail.com>
2025-12-04 15:17:43 +0100

    refactor: remove HelloWorld component and update cart store and checkout view for consistency

--- v-plain/src/views/Checkout.vue
+++ v-plain/src/views/Checkout.vue
@@ -1,32 +1,30 @@
 <script setup lang="ts">
-import { computed } from 'vue'
-import { useCartStore } from '../store/cart.ts'
-import type {Activity} from '../data/Activity.ts'
+import { computed } from "vue";
+import { useCartStore } from "../store/cart.ts";
+import type { Activity } from "../data/Activity.ts";
 
-const cartStore = useCartStore()
-
+const cartStore = useCartStore();
 
 const totalPrice = computed((): number => {
   return cartStore.cart.items.reduce((sum: number, item: Activity) => {
-    return sum + item.price
-  }, 0)
-})
+    return sum + item.price;
+  }, 0);
+});
 
 const buyAll = (): void => {
-  console.log("Bought all items")
-}
+  console.log("Bought all items");
+};
 </script>
 
 <template>
-  
   <h1>Checkout</h1>
   <div class="checkout-container">
     <ul class="checkout-lista">
-      <li v-for="item in cartStore.cart.items" :key="item.activityId">
+      <li v-for="item in cartStore.cart.items" :key="item.id">
         <h3>{{ item.title }}</h3>
         <p>{{ item.description }}</p>
         <p>{{ item.price }}</p>
-        <button @click="cartStore.removeItem(item.activityId)">Remove Item</button>
+        <button @click="cartStore.removeItem(item.id)">Remove Item</button>
       </li>
     </ul>
     <p>{{ totalPrice }}</p>
