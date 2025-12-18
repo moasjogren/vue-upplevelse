e759f8cede21e5442d35777d9bc7ce2991b5e96e
Moa Sjögren <moasjgren@gmail.com>
2025-12-18 15:08:38 +0100

    Småfix, cart funkar som den ska, tagit bort överflödiga kommentarer

--- v-plain/src/App.vue
+++ v-plain/src/App.vue
@@ -1,9 +1,7 @@
 <script setup lang="ts">
-import Header from "./components/Header.vue"
-import Footer from "./components/Footer.vue"
+import Header from "./components/Header.vue";
+import Footer from "./components/Footer.vue";
 </script>
-
-<!-- Enkel setup för Vue router utifrån Joels basic-exempel. Lägg till sidor i /views och länka i /router -->
 
 <template>
   <Header></Header>
@@ -18,13 +16,12 @@ nav {
   display: flex;
   gap: 1rem;
   padding: 1rem;
-  /* background-color: #f0f0f0; */
   margin-bottom: 2rem;
 }
 
 .router-link-active {
   font-weight: bold;
-  color: #42b883; /* Vue green */
+  color: #42b883;
   text-decoration: underline;
 }
 </style>
